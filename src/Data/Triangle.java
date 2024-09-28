9a3ed905591bb02a7434cb431a651fe860b8a284
Alejo Madrid Giral <alejomadridg@hotmail.com>
2024-09-28 08:20:38 -0500

    calculate perimeters for rectagle and triangle

--- src/Data/Triangle.java
+++ src/Data/Triangle.java
@@ -3,7 +3,6 @@ package Data;
 public class Triangle {
 
     public float baseSize;
-    public float perpendicularHeightSize;
     public float aSideSize;
     public float cSideSize;
 
--- src/Test/Runner.java
+++ src/Test/Runner.java
@@ -1,6 +1,8 @@
 package Test;
 
+import Data.Rectangle;
 import Data.Square;
+import Data.Triangle;
 
 import java.util.Scanner;
 
@@ -15,6 +17,32 @@ public class Runner {
         mySquare.sideSize = squareSide;
 
         System.out.println("El perimetro del cuadrado es: " + mySquare.getPerimeter());
+
+        System.out.println("por favor indique la longitud del rectangulo");
+        float rectangleLength = scan.nextFloat();
+        Rectangle myRectangle = new Rectangle();
+        myRectangle.lengthSize = rectangleLength;
+
+        System.out.println("Por favor indique el ancho del rectangulo");
+        float rectangleBreadht = scan.nextFloat();
+        myRectangle.breadthSize = rectangleBreadht;
+
+        System.out.println("El perimetro del rectangulo es: " + myRectangle.getPerimeter());
+
+        System.out.println("Por favor indique la longitud de la base del trianqulo");
+        float triangleBase = scan.nextFloat();
+        Triangle myTriangle = new Triangle();
+        myTriangle.baseSize = triangleBase;
+
+        System.out.println("Por favor indique la longitud del lado a trianqulo");
+        float trianglASide = scan.nextFloat();
+        myTriangle.aSideSize = trianglASide;
+
+        System.out.println("Por favor indique la longitud del lado c trianqulo");
+        float triangleCside = scan.nextFloat();
+        myTriangle.cSideSize = triangleCside;
+
+        System.out.println("El perimetro del triangulo es: " + myTriangle.getPerimeter());
 
     }
 
